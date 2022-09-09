5a3cf2b39c118fb434fd3425c5351c7fb2686e3c
Thais Bertoldo <thaisbertoldo11@gmail.com>
2022-09-08 22:11:34 -0300

    feat: add memo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import { BrowserRouter } from 'react-router-dom';
 import { Footer, Header } from './components';
 import { Router } from './routes/Router';
 
 function App() {
+  const [teste, setTeste] = useState(false);
+
   return (
     <BrowserRouter>
       <div className='d-flex flex-column justify-content-between min-vh-100 bg-light'>
@@ -11,7 +14,8 @@ function App() {
         <Container as='main' className='my-5'>
           <Router />
         </Container>
-        <Footer />
+        <button onClick={() => setTeste(!teste)}>Teste</button>
+        <Footer teste={teste} />
       </div>
     </BrowserRouter>
   );
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,11 @@
-export const Footer = () => (
+import { Teste } from '../Teste/Teste';
+
+export const Footer = ({ teste }) => (
   <footer className='bg-dark text-light p-3 d-flex gap-1 justify-content-center'>
     © {new Date().getFullYear()} Copyright:
     <a className='text-light' href='https://devinhouse.tech/'>
-      DEVinHouse
+      DEVinHouse {teste ? 'Teste' : ''}
     </a>
+    <Teste nome='DevInHouse' />
   </footer>
 );
